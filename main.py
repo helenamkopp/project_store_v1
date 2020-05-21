6136063a84a1dbaf960a08231a2f3a49f149c484
Helena <helena.mkopp@gmail.com>
2020-05-21 15:20:32 -0300

    end project

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-class Product:
+class Payment:
 
-    def __init__(self, name_product, price):
+    def __init__(self, form_of_payment, payment_installments=0):
+        self._form_of_payment = str(form_of_payment)
+        self._payment_installments = int(payment_installments)
+
+
+class Product(Payment):
+
+    def __init__(self, name_product, price, form_of_payment, payment_installments=0):
+        super().__init__(form_of_payment, payment_installments)
+
+        if self._form_of_payment != "money" and self._form_of_payment != "credit" and self._form_of_payment != "debit":
+            raise NameError(f"form_of_payment must be money or credit or debit not {self._form_of_payment}")
+
         self._name_product = str(name_product)
         self._price = float(price)
 
+    def apply_discount(self):
+        if self._form_of_payment == "money":
+            self._price -= self._price * 0.20
+            return self._price
+        else:
+            return False
 
-class Payment:
+    def apply_interest(self):
+        if self._form_of_payment == "credit" and self._payment_installments <= 2 or self._form_of_payment == "debit":
+            new_price = self._price
 
-    def __init__(self, form_of_payment):
-        self._form_of_payment = str(form_of_payment)
+        elif self._form_of_payment == "credit" and self._payment_installments <= 4:
+            new_price = self._price * 1.05
+
+        elif self._form_of_payment == "credit" and self._payment_installments >= 5:
+            new_price = self._price * 1.10
+
+        return float(f"{new_price:.2f}")
 
 
 class Client:
 
-    def __init__(self, name_client, cpf, product, form_of_payment):
+    def __init__(self, name_client, cpf, product):
         if not isinstance(product, Product):
             raise TypeError(f"product must be of type Product not {type(product)}")
 
-        if not isinstance(form_of_payment, Payment):
-            raise TypeError(f"form_of_payment must be of type Payment not {type(form_of_payment)}")
-
-        self._name_client = name_client
-        self._cpf = cpf
-    #
-    #
-    # def apply_discount(self):
-    #     if self._form_of_payment == "money":
-    #         self._price -= (self._price * 0.20)
-    #
-    #     elif self._form_of_payment == "credit card".strip().upper():
-    #         self._price += (self._price + 1.05)
-    #
-    #     elif self._form_of_payment == "debit card".strip().upper():
-    #         self._price -= (self._price * 0.05)
-    #
-    #
+        self._name_client = str(name_client)
+        self._cpf = str(cpf)
--- test_main.py
+++ test_main.py
@@ -1,8 +1,54 @@
-from main import Product, Payment, Client
+import unittest
+from main import Client, Payment, Product
 
-product_1 = Product("T-Shirt", 125.60)
-product_2 = Product("Pants", 80.00)
-product_3 = Product("Shoes", 250.00)
 
-client_1 = Client("Helena", "555.444.111-95", product_1)
+class TestMain(unittest.TestCase):
 
+    def setUp(self):
+        self.product_1 = Product("Blouse", 250.00, "money")
+        self.product_2 = Product("Pants", 190.00, "credit", 3)
+        self.product_3 = Product("Jacket", 625.50, "credit", 8)
+        self.product_4 = Product("Shoes", 325.75, "debit")
+        self.product_5 = Product("Bikini", 65.80, "credit", 1)
+
+        self.client_1 = Client("Isabel", "222.222.222-22", self.product_1)
+        self.client_2 = Client("Maria", "777.555.111-65", self.product_2)
+        self.client_3 = Client("Gabriel", "111.333.666-41", self.product_3)
+        self.client_4 = Client("Mariana", "666.555.777-10", self.product_4)
+        self.client_5 = Client("Lucia", "444.444.444-44", self.product_5)
+
+    def tearDown(self):
+        pass
+
+    def test_apply_discount(self):
+        self.product_1.apply_discount()
+
+        self.assertEqual(self.product_1._price, 200.00)
+        self.assertIsNot(self.product_1._price, 300.00)
+
+    def test_apply_interest(self):
+
+        self.assertEqual(self.product_2.apply_interest(), 199.50)
+        self.assertEqual(self.product_3.apply_interest(), 688.05)
+        self.assertEqual(self.product_4.apply_interest(), 325.75)
+        self.assertEqual(self.product_5.apply_interest(), 65.80)
+
+    def test_client_name(self):
+
+        self.assertEqual(self.client_1._name_client, "Isabel")
+        self.assertIsNot(self.client_2._name_client, "Helena")
+        self.assertEqual(self.client_3._name_client, "Gabriel")
+
+    def test_payment_installments(self):
+
+        self.assertEqual(self.product_1._payment_installments, 0)
+        self.assertEqual(self.product_2._payment_installments, 3)
+        self.assertIsNot(self.product_3._payment_installments, 7)
+        self.assertEqual(self.product_4._payment_installments, 0)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
